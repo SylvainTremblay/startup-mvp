8b63fef20049e32e2f722d73e4d174400a423d6d
SylvainYYC <sylvain@csofdv.solutions>
2025-06-03 19:53:26 -0600

    renaming to StartupMvp

--- startup-mvp-api/src/main/java/com/csofdv/bmvpf/StartupMvpApplication.java
+++ startup-mvp-api/src/main/java/com/csofdv/bmvpf/StartupMvpApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuildMvpFastApplication {
+public class StartupMvpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuildMvpFastApplication.class, args);
+		SpringApplication.run(StartupMvpApplication.class, args);
 	}
 
 }
